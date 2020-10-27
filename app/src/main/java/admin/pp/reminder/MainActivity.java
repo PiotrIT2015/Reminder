353299465205ce18f2a80ceec6500506c91eec05
PiotrIT2017 <piotr.prochnicki@gmail.com>
2020-10-27 04:11:22 +0100

    Feature-fix/delete method

--- app/src/main/java/admin/pp/reminder/MainActivity.java
+++ app/src/main/java/admin/pp/reminder/MainActivity.java
@@ -105,64 +105,7 @@ public class MainActivity extends AppCompatActivity{
         return selectSql;
     }
 
-    void InsertQuery() {
-        String connectionString =
-                "jdbc:sqlserver://sqlserverapp.database.windows.net:1433;"
-                        + "database=database1;"
-                        + "user=Gekon@sqlserverapp.database.windows.net;"
-                        + "password=pass;"
-                        + "encrypt=true;"
-                        + "trustServerCertificate=false;"
-                        + "hostNameInCertificate=*.database.windows.net;"
-                        + "loginTimeout=30;";
-
-        // Declare the JDBC objects.
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
-        PreparedStatement prepsInsertProduct = null;
-
-        try {
-            connection = DriverManager.getConnection(connectionString);
-
-            // Create and execute an INSERT SQL prepared statement.
-            String insertSql = "INSERT INTO SalesLT.Product (Name, ProductNumber, Color, StandardCost, ListPrice, SellStartDate) VALUES "
-                    + "('NewBike', 'BikeNew', 'Blue', 50, 120, '2016-01-01');";
-
-            prepsInsertProduct = connection.prepareStatement(
-                    insertSql,
-                    Statement.RETURN_GENERATED_KEYS);
-            prepsInsertProduct.execute();
-
-            // Retrieve the generated key from the insert.
-            resultSet = prepsInsertProduct.getGeneratedKeys();
-
-            // Print the ID of the inserted row.
-            while (resultSet.next()) {
-                System.out.println("Generated: " + resultSet.getString(1));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // Close the connections after the data has been handled.
-            if (prepsInsertProduct != null) try {
-                prepsInsertProduct.close();
-            } catch (Exception e) {
-            }
-            if (resultSet != null) try {
-                resultSet.close();
-            } catch (Exception e) {
-            }
-            if (statement != null) try {
-                statement.close();
-            } catch (Exception e) {
-            }
-            if (connection != null) try {
-                connection.close();
-            } catch (Exception e) {
-            }
-        }
-    }
+    
 
 
 }
